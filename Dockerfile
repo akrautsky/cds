f7dd16e2670e049a04431650606d38fd3cbb6f60
Akash Raut <avraut@usf.edu>
2022-08-20 11:02:25 -0400

    New project with new installs in new env

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.8.10
+
+COPY . /app
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+EXPOSE $PORT
+
+CMD gunicorn --worker=4 --bind 0.0.0.0:$PORT app:app
---
+++ app.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+
+app = Flask(__name__)
+
+my_saved_model = tf.keras.models.load_model(
+    ('my_cds.h5'),
+    custom_object = {'KerasLayer': hub.KerasLayer}
+)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
